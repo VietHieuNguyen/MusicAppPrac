1f7490588db835bf0d2546075833593a5912e23e
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-26 12:10:27 +0700

    B42 1 5.1 + 5.2

--- middleware/admin/uploadCloud.middleware.ts
+++ middleware/admin/uploadCloud.middleware.ts
@@ -1,36 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { v2 as cloudinary } from "cloudinary";
-import streamifier from "streamifier";
-import dotenv from "dotenv"
-dotenv.config()
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API,
-  api_secret: process.env.CLOUD_SECRET
-});
-
-const streamUpload = (buffer: Buffer): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    let stream = cloudinary.uploader.upload_stream(
-      { resource_type: 'auto' }, 
-      (error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          reject(error);
-        }
-      }
-    );
-
-    streamifier.createReadStream(buffer).pipe(stream);
-  });
-};
-
-const uploadToCloudinary = async (buffer: any) => {
-  let result = await streamUpload(buffer);
-  return result["url"];
-};
-
+import { uploadToCloudinary } from "../../helpers/uploadToCloudianry";
 export const uploadSingle = async (req: Request, res: Response, next: NextFunction) => {
   try {
     if (req["file"]) {
@@ -42,3 +11,19 @@ export const uploadSingle = async (req: Request, res: Response, next: NextFuncti
   }
   next();
 };
+export const uploadFields = async (req: Request, res: Response, next: NextFunction)=>{
+  for(const key in req["files"]){
+    req.body[key] = [];
+
+    const array = req["files"][key];
+    for(const item of array){
+      try {
+        const result = await uploadToCloudinary(item.buffer);
+        req.body[key].push(result)
+      } catch (error) {
+        
+      }
+    }
+  }
+  next();
+}
--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -8,7 +8,10 @@ router.get("/", controller.index)
 
 router.get("/create", controller.create)
 
-router.post("/create", upload.single("avatar"),uploadCloud.uploadSingle, controller.createPost)
+router.post("/create", upload.fields([
+  { name: "avatar", maxCount: 1 }, 
+  { name: "audio", maxCount: 1}
+]), uploadCloud.uploadFields, controller.createPost)
 
 
 
